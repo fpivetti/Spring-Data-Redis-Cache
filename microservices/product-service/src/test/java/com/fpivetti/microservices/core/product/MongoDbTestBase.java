a0f538840da48be03fac1a97570a6d33239ca503
fpivetti <frencipivo@gmail.com>
2024-04-21 19:52:22 +0200

    Update PersistenceTests in product service

--- microservices/product-service/src/test/java/com/fpivetti/microservices/core/product/MongoDbTestBase.java
+++ microservices/product-service/src/test/java/com/fpivetti/microservices/core/product/MongoDbTestBase.java
@@ -1,30 +1,14 @@
 package com.fpivetti.microservices.core.product;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MongoDBContainer;
 
 public abstract class MongoDbTestBase {
-    static MongoDBContainer database = new MongoDBContainer (
-            "mongo:latest"
-    );
 
-    @BeforeAll
-    static void beforeAll() {
+    @ServiceConnection
+    private static final MongoDBContainer database = new MongoDBContainer("mongo:6.0.4");
+
+    static {
         database.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        database.stop();
-    }
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.host", database::getHost);
-        registry.add("spring.data.mongodb.port", () -> database.getMappedPort(27017));
-        registry.add("spring.data.mongodb.database", () -> "test");
     }
 }
--- microservices/product-service/src/test/java/com/fpivetti/microservices/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/com/fpivetti/microservices/core/product/PersistenceTests.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 
 @DataMongoTest
 class PersistenceTests extends MongoDbTestBase{
+
     @Autowired
     private ProductRepository repository;
     private ProductEntity savedEntity;
