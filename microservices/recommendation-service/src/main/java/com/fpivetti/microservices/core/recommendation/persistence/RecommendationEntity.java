ec94e602e0d32cef87862249c08c4c62a26874b8
fpivetti <frencipivo@gmail.com>
2024-04-21 16:53:59 +0200

    Add entity classes to the persistence layer

---
+++ microservices/recommendation-service/src/main/java/com/fpivetti/microservices/core/recommendation/persistence/RecommendationEntity.java
@@ -0,0 +1,88 @@
+package com.fpivetti.microservices.core.recommendation.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "recommendations")
+@CompoundIndex(name = "recommendations_unique_idx", unique = true, def = "{'productId': 1, 'recommendationId'; 1}")
+public class RecommendationEntity {
+    @Id
+    private String id;
+    @Version
+    private Integer version;
+    private int productId;
+    private int recommendationId;
+    private String author;
+    private int rate;
+    private String content;
+
+    public RecommendationEntity() {}
+
+    public RecommendationEntity(String id, Integer version, int productId, int recommendationId, String author, int rate, String content) {
+        this.id = id;
+        this.version = version;
+        this.productId = productId;
+        this.recommendationId = recommendationId;
+        this.author = author;
+        this.rate = rate;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getRecommendationId() {
+        return recommendationId;
+    }
+
+    public void setRecommendationId(int recommendationId) {
+        this.recommendationId = recommendationId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
