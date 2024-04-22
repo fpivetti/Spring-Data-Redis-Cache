c1e86220c8b9545d81673a05113efcf46ae13ca5
fpivetti <frencipivo@gmail.com>
2024-04-22 19:36:32 +0200

    Add databases to the Docker Compose landscape

--- microservices/product-composite-service/src/test/java/com/fpivetti/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/com/fpivetti/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -3,9 +3,13 @@ package com.fpivetti.microservices.composite.product;
 import static java.util.Collections.singletonList;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.just;
 
+import com.fpivetti.api.composite.product.ProductAggregateDto;
+import com.fpivetti.api.composite.product.RecommendationSummaryDto;
+import com.fpivetti.api.composite.product.ReviewSummaryDto;
 import com.fpivetti.api.core.product.ProductDto;
 import com.fpivetti.api.core.recommendation.RecommendationDto;
 import com.fpivetti.api.core.review.ReviewDto;
@@ -17,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -47,14 +52,32 @@ class ProductCompositeServiceApplicationTests {
 	}
 
 	@Test
+	void createCompositeProduct1() {
+		ProductAggregateDto compositeProduct = new ProductAggregateDto(1, "name", 1, null, null, null);
+		postAndVerifyProduct(compositeProduct, OK);
+	}
+
+	@Test
+	void createCompositeProduct2() {
+		ProductAggregateDto compositeProduct = new ProductAggregateDto(1, "name", 1,
+				singletonList(new RecommendationSummaryDto(1, "a", 1, "c")),
+				singletonList(new ReviewSummaryDto(1, "a", "s", "c")), null);
+		postAndVerifyProduct(compositeProduct, OK);
+	}
+
+	@Test
+	void deleteCompositeProduct() {
+		ProductAggregateDto compositeProduct = new ProductAggregateDto(1, "name", 1,
+				singletonList(new RecommendationSummaryDto(1, "a", 1, "c")),
+				singletonList(new ReviewSummaryDto(1, "a", "s", "c")), null);
+		postAndVerifyProduct(compositeProduct, OK);
+		deleteAndVerifyProduct(compositeProduct.getProductId(), OK);
+		deleteAndVerifyProduct(compositeProduct.getProductId(), OK);
+	}
+
+	@Test
 	void getProductById() {
-		client.get()
-				.uri("/product-composite/" + PRODUCT_ID_OK)
-				.accept(APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isOk()
-				.expectHeader().contentType(APPLICATION_JSON)
-				.expectBody()
+		getAndVerifyProduct(PRODUCT_ID_OK, OK)
 				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
 				.jsonPath("$.recommendations.length()").isEqualTo(1)
 				.jsonPath("$.reviews.length()").isEqualTo(1);
@@ -62,27 +85,40 @@ class ProductCompositeServiceApplicationTests {
 
 	@Test
 	void getProductNotFound() {
-		client.get()
-				.uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
-				.accept(APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isNotFound()
-				.expectHeader().contentType(APPLICATION_JSON)
-				.expectBody()
+		getAndVerifyProduct(PRODUCT_ID_NOT_FOUND, NOT_FOUND)
 				.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
 				.jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
 	}
 
 	@Test
 	void getProductInvalidInput() {
-		client.get()
-				.uri("/product-composite/" + PRODUCT_ID_INVALID)
-				.accept(APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
-				.expectHeader().contentType(APPLICATION_JSON)
-				.expectBody()
+		getAndVerifyProduct(PRODUCT_ID_INVALID, UNPROCESSABLE_ENTITY)
 				.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
 				.jsonPath("$.message").isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		return client.get()
+				.uri("/product-composite/" + productId)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody();
+	}
+
+	private void postAndVerifyProduct(ProductAggregateDto compositeProduct, HttpStatus expectedStatus) {
+		client.post()
+				.uri("/product-composite")
+				.body(just(compositeProduct), ProductAggregateDto.class)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus);
+	}
+
+	private void deleteAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		client.delete()
+				.uri("/product-composite/" + productId)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus);
 	}
 }
